e046f409282fa2afe006852538f946d5ac2fef58
Emily Morgado <emilyannemorgado@gmail.com>
2019-07-23 05:25:24 -0700

    Updates

---
+++ precache-manifest.78d87d9cf45456a74ba8eec5bf4a872e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c603ec4ecb50447c2a98171ca5337331",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "7d887a797b20c485801f",
+    "url": "/monster-rolodex/static/css/main.bb4f2ecb.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/monster-rolodex/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "7d887a797b20c485801f",
+    "url": "/monster-rolodex/static/js/main.6ed01cd8.chunk.js"
+  },
+  {
+    "revision": "344166cfeaf30c2159bd",
+    "url": "/monster-rolodex/static/js/runtime~main.5df944a8.js"
+  }
+]);
